5d5bcdcfd6cf7541ad725a6b83e3230bf0721532
KaMa75 <karolmarszalek@o2.pl>
2020-11-23 19:57:06 +0100

    users data mock, users components

---
+++ src/components/Users/UsersContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UsersContainer() {
+    return (
+        <div>
+            User
+        </div>
+    );
+}
+
+export default UsersContainer;
---
+++ src/components/Users/index.js
@@ -0,0 +1,4 @@
+export {default} from './UsersContainer';
+
+export {default as User} from './User';
+export {default as UsersList} from './UsersList';
