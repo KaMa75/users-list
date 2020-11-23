aa42787c9859106feb08b7da568ab7ba3793a2b5
KaMa75 <karolmarszalek@o2.pl>
2020-11-23 20:16:32 +0100

    fetching users data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
+import UsersContainer from './components/Users';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      asd
+      <UsersContainer />
     </div>
   );
 }
--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 function UsersContainer() {
+    const [usersList, setUsersList] = useState();
+
+    useEffect(() => {
+        fetch('users.json')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            });
+    });
+
+    console.log(usersList);
+
     return (
         <div>
             User
