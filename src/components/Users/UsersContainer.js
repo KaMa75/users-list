50d7df76fa083ce0824dec09b5b6e2e289692c9c
KaMa75 <karolmarszalek@o2.pl>
2020-11-23 20:17:38 +0100

    fetching catch error

--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -8,6 +8,9 @@ function UsersContainer() {
             .then(res => res.json())
             .then(data => {
                 console.log(data)
+            })
+            .catch(err => {
+                console.log(err)
             });
     });
 
