cf1d521d31606015010b87bf623379bc337826de
KaMa75 <karolmarszalek@o2.pl>
2020-11-29 18:56:13 +0100

    user detail page

--- src/components/UserDetail/UserDetail.js
+++ src/components/UserDetail/UserDetail.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useHistory} from 'react-router-dom';
-import {Button} from '@material-ui/core';
+import {Button, Grid, List, ListItem, ListItemText, Typography} from '@material-ui/core';
 
 import {DataNotFound} from '../EmptyStates';
 
@@ -15,26 +15,113 @@ function UserDetail({user}) {
 
     const renderData = () => {
         if(user && Object.keys(user).length > 0) {
-            return <p>są dane</p>
+            return (
+                <>
+                    <Grid
+                        xs={12}
+                    >
+                        <Typography
+                            variant="h6"
+                        >
+                            Dane użytkownika
+                        </Typography>
+                    </Grid>
+                    <Grid
+                        item
+                        xs={12}
+                        md={6}
+                    >
+                        <List>
+                            <ListItem>
+                                <ListItemText
+                                    primary={user.first_name}
+                                    secondary="Imię"
+                                />
+                            </ListItem>
+                        </List>
+                    </Grid>
+                    <Grid
+                        item
+                        xs={12}
+                        md={6}
+                    >
+                        <List>
+                            <ListItem>
+                                <ListItemText
+                                    primary={user.last_name}
+                                    secondary="Nazwisko"
+                                />
+                            </ListItem>
+                        </List>
+                    </Grid>
+                    <Grid
+                        item
+                        xs={12}
+                        md={6}
+                    >
+                        <List>
+                            <ListItem>
+                                <ListItemText
+                                    primary={user.email}
+                                    secondary="E-mail"
+                                />
+                            </ListItem>
+                        </List>
+                    </Grid>
+                    <Grid
+                        item
+                        xs={12}
+                        md={6}
+                    >
+                        <List>
+                            <ListItem>
+                                <ListItemText
+                                    primary={user.gender}
+                                    secondary="Płeć"
+                                />
+                            </ListItem>
+                        </List>
+                    </Grid>
+                    <Grid
+                        item
+                        xs={12}
+                        md={6}
+                    >
+                        <List>
+                            <ListItem>
+                                <ListItemText
+                                    primary={user.ip_address}
+                                    secondary="Adres IP"
+                                />
+                            </ListItem>
+                        </List>
+                    </Grid>
+                </>
+            );
         }
         return  <DataNotFound />
     }
 
     return (
-        <div>
-            <div>
-                {renderData()}
-            </div>
-            <div>
+        <Grid
+            container
+        >
+            {renderData()}
+            <Grid
+                xs={12}
+            >
                 <Button
+                    style={{marginTop: 50}}
                     variant="outlined"
                     color="primary"
                     onClick={handleOnClick}
                 >
                     Lista użytkowników
                 </Button>
-            </div>
-        </div>
+            </Grid>
+        </Grid>
+
+
     );
 }
 
